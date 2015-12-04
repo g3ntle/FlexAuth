99f54b1c262a9b3396f22d2366614c75a3603d5d
robhendriks <cyanidenl@icloud.com>
2015-12-04 14:29:03 +0100

    Moar dummy

---
+++ FlexAuth/FlexAuth/Security/Generic/DummyCredentials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FlexAuth.Security.Generic
+{
+    public class DummyCredentials : ICredentials
+    {
+        #region Properties
+
+        public object MetaData { get; set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public bool Check()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
