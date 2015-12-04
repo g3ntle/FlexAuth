02b9fe6be095573d9b633efe37887dce3069cb4e
robhendriks <cyanidenl@icloud.com>
2015-12-04 01:12:43 +0100

    Created Dummy repository

---
+++ FlexAuth/FlexAuth/Security/Generic/DummyNodeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlexAuth.Security.Generic
+{
+    public class DummyNodeRepository : INodeRepository
+    {
+        public IEnumerable<string> Nodes { get; set; }
+
+        public bool HasNode(string node)
+        {
+            return true;
+        }
+    }
+}
